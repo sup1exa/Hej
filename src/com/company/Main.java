08e496b154387438d64d25a3691d74e94cd53043
Student <alexkuts5b@gmail.com>
2023-02-28 16:12:40 +0100

    initail commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,8 @@
 package com.company;
 
 public class Main {
+
     public static void main(String[] args) {
-        //1234241
+       point point1 = new point(12.5,23.3);
     }
 }
---
+++ src/com/company/point.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class point {
+    public final double x,y;
+
+    public point(double x,double y){
+        this.x=x;
+        this.y=y;
+    }
+}
